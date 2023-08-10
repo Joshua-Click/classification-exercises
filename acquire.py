c5853224d6537f9cea065df7f6959429636445c8
Joshua-Click <joshua.click25@gmail.com>
2023-08-10 16:57:56 -0500

    one last push for the day

--- acquire.py
+++ acquire.py
@@ -1,13 +1,37 @@
-def get_titanic_data
+import env
+import os
+import pandas as pd
+
+# make a funcion that reads content in from sql
+def grab_data(
+    db,
+    user=env.user,
+    password=env.password,
+    host=env.host):
+    '''
+    grab data will query data from a specified positional argument (string literal)
+    schema from an assumed user, password, and host provided
+    that they were imported from an env
+    
+    return: a pandas dataframe
+    '''
+    query = '''SELECT * FROM employees LIMIT 100'''
+    connection = f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    df = pd.read_sql(query, connection)
+    return df
+
+
+def get_titanic_data():
+    pass
 
 
 
 
-
-def get_iris_data
-
-
+def get_iris_data():
+    pass
 
 
 
-def get_telco_data
+
+def get_telco_data():
+    pass
