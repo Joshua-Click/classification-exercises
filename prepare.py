76f435c3b29b1e3b3fd99dfffb5fca0b78b707b1
Joshua-Click <joshua.click25@gmail.com>
2023-08-14 10:47:05 -0500

    added function

---
+++ prepare.py
@@ -0,0 +1,16 @@
+def split_data(df, target_variable):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target_variable])
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate[target_variable])
+    
+    print(f'Train: {len(train)/len(df)}')
+    print(f'Validate: {len(validate)/len(df)}')
+    print(f'Test: {len(test)/len(df)}')
+
+    return train, validate, test
