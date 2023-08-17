a029149d55c7484a6619c224d84ee9dd9182fbe3
Joshua-Click <joshua.click25@gmail.com>
2023-08-17 15:13:27 -0500

    made changes to prep, decision tree, and finished random forest

--- prepare.py
+++ prepare.py
@@ -1,3 +1,8 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+
 def split_data(df, target_variable):
     '''
     take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
@@ -13,4 +18,100 @@ def split_data(df, target_variable):
     print(f'Validate: {len(validate)/len(df)}')
     print(f'Test: {len(test)/len(df)}')
 
+    return train, validate, test
+
+def get_metrics(model,xtrain,ytrain,xtest,ytest):
+    
+    labels = sorted(y_train.unique())
+
+    # OUTPUTS AN ARRAY OF PREDICTIONS
+    preds = model.predict(xtest)
+    print("Accuracy Score:", model.score(xtest,ytest))
+    print()
+    print('Confusion Matrix:')
+    conf = confusion_matrix(ytest,preds)
+    conf = pd.DataFrame(conf,
+            index=[str(label) + '_actual'for label in labels],
+            columns=[str(label) + '_predict'for label in labels])
+    print(conf)
+    print()
+    print("Classification Report:")
+    print(classification_report(ytest, preds))
+
+def clean_titanic1(df):
+    '''
+    This function will take in a dataframe
+    drop unwanted columns
+    and impute age and embarked columns
+    Additionally, it will create dummy columns
+    then return a clean dataframe
+    '''
+    #drop columns
+    df = df.drop(columns =['embark_town','class','deck'])
+    #impute columns
+    df['age'] = df['age'].fillna(df.age.mean())
+    df['embarked'] = df['embarked'].fillna(value='S')
+    #create dummies
+    dummy_df = pd.get_dummies(df[['sex','embarked']], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+
+
+
+
+def split_titanic_data(df, target='survived'):
+    '''
+    split titanic data will split data based on 
+    the values present in a cleaned version of titanic
+    that is from clean_titanic
+    
+    '''
+    train_val, test = train_test_split(df,
+                                   train_size=0.8,
+                                   random_state=1108,
+                                   stratify=df[target])
+    train, validate = train_test_split(train_val,
+                                   train_size=0.7,
+                                   random_state=1108,
+                                   stratify=train_val[target])
+    return train, validate, test
+
+def clean_titanic(df):
+    '''
+    clean titanic will take in a single pandas dataframe
+    and will proceed to drop redundant columns
+    and nonuseful information
+    in addition to addressing null values
+    and encoding categorical variables
+    '''
+    #drop out any redundant, excessively empty, or bad columns
+    df = df.drop(columns=['passenger_id','embarked','deck','class'])
+    # impute average age and most common embark_town:
+    train, validate, test = split_titanic_data(df)
+    # impute missing values for our fields using sklearn's simpleimputer
+    #create age imputer, with strategry mean
+    my_age_imputer = SimpleImputer(strategy='mean')
+    # use imputer object to fit to train ages
+    my_age_imputer.fit(train[['age']])
+    # tranform values in train, validate, and test based on mean fit from the last step
+    train.loc[:,'age'] = my_age_imputer.transform(train[['age']])
+    validate.loc[:,'age'] = my_age_imputer.transform(validate[['age']])
+    test.loc[:,'age'] = my_age_imputer.transform(test[['age']])     
+    # go through the same process with embark_town
+    my_embark_imputer = SimpleImputer(strategy='most_frequent')
+    my_embark_imputer.fit(train[['embark_town']])
+    train.loc[:,'embark_town'] = my_embark_imputer.transform(train[['embark_town']])
+    validate.loc[:,'embark_town'] = my_embark_imputer.transform(validate[['embark_town']])
+    test.loc[:,'embark_town'] = my_embark_imputer.transform(test[['embark_town']])
+    # encode categorical values: 
+    train = pd.concat(
+    [train, pd.get_dummies(train[['sex', 'embark_town']],
+                        drop_first=True)], axis=1)
+    validate = pd.concat(
+    [validate, pd.get_dummies(validate[['sex', 'embark_town']],
+                        drop_first=True)], axis=1)
+    test = pd.concat(
+    [test, pd.get_dummies(test[['sex', 'embark_town']],
+                        drop_first=True)], axis=1)                                                  
     return train, validate, test
